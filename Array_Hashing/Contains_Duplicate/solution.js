d351e2804516d523fc5b6828cf7aa8b8db982c1d
al fahim bin faruk <fmd03700@gmail.com>
2022-10-24 01:23:18 +0600

    updated

--- Array_Hashing/Contains_Duplicate/solution.js
+++ Array_Hashing/Contains_Duplicate/solution.js
@@ -24,17 +24,97 @@ Constraints:
 
 ****/
 
-const containsDuplicate=(nums)=>{
-    hashSet = new Set()
+/**
+ * Solution 01:
+ * Hash Set - Early Exit
+ * Time O(N) | Space O(N)
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicateSolutionOne = (nums) => {
+  hashSet = new Set();
 
-    for(let i=0 ; i<=nums.length ; i++){
-        if(hashSet.has(nums[i])){
-            return true
-        }
-        hashSet.add(nums[i])
+  for (const num of nums) {
+    if (hashSet.has(num)) {
+      return true;
     }
-    return false
-}
+    hashSet.add(num);
+  }
+  return false;
+};
 
-console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]))
-console.log(containsDuplicate([1, 2, 3, 4]))
+console.log(containsDuplicateSolutionOne([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+console.log(containsDuplicateSolutionOne([1, 2, 3, 4]));
+
+/**
+ * Solution 02:
+ * Hash Set
+ * Time O(N) | Space O(N)
+ * https://leetcode.com/problems/contains-duplicate/
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicateSolutionTwo = (nums) => {
+  hashSet = new Set(nums);
+
+  if (hashSet.size === nums.length) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+console.log(containsDuplicateSolutionTwo([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+console.log(containsDuplicateSolutionTwo([1, 2, 3, 4]));
+
+/**
+ * Solution 03:
+ * Brute Force - Linear Search
+ * Time O(N^2) | Space O(1)
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicateSolutionThree = (nums) => {
+  for (let right = 0; right < nums.length; right++) {
+    for (let left = 0; left < right; left++) {
+      const isDuplicate = nums[left] === nums[right];
+      if (isDuplicate) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+console.log(containsDuplicateSolutionThree([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+console.log(containsDuplicateSolutionThree([1, 2, 3, 4]));
+
+/**
+ * Solution 04:
+ * Sort - HeapSort Space O(1) | QuickSort Space O(log(N))
+ * Time O(N * log(N)) | Space O(1)
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicateSolutionFour = (nums) => {
+  // this will sort the array
+  nums.sort((a, b) => a - b);
+
+  return hasDuplicate(nums)
+};
+
+const hasDuplicate = (nums) => {
+  for (let current = 0; current < nums.length - 1; current++) {
+    const next = current + 1;
+
+    const isDuplicate = nums[current] == nums[next];
+
+    if (isDuplicate) {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log(containsDuplicateSolutionFour([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+console.log(containsDuplicateSolutionFour([1, 2, 3, 4]));
