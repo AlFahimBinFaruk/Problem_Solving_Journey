cf294050d3ea208c53eda90a4194de0bb40eb10f
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-11-21 19:21:26 +0600

    updated

--- Two_Pointers/3Sum/solution.py
+++ Two_Pointers/3Sum/solution.py
@@ -34,30 +34,34 @@ Constraints:
 -105 <= nums[i] <= 105
 """
 
-# Time O(N ^ 2) | Space O(N)
-def threeSumSolution(nums):
-    result=[]
-    nums.sort()
-    
-    for i,n in enumerate(nums):
-        if i>0 and nums[i-1]==n:
-            continue
-        l=i+1
-        r=len(nums)-1
-        while l<r:
-            threeSum=n+nums[l]+nums[r]
-            if threeSum < 0 :
-                l+=1
-            elif threeSum > 0 :
-                r -=1
-            else:
-                result.append([n,nums[l],nums[r]])
-                l+=1
-                while(l<r and nums[l]==nums[l-1]):
-                    l+=1
+class Solution:
+    # Time O(N^2) | Space O(N)
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # in 3sum we will also use the logic of 2sum
+        result = []
+        # sort the input in assending order
+        nums.sort()
 
-    return result    
+        for i,n in enumerate(nums):
+            # if the value is already checked
+            if i > 0 and nums[i-1] == n:
+                continue
 
+            l = i+1
+            r = len(nums)-1
 
-print(threeSumSolution([-1, 0, 1, 2, -1, -4]))
-print(threeSumSolution([0, 0, 0]))
+            while l < r:
+                total = n + nums[l] + nums[r]
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -=1
+                else:
+                    result.append([n,nums[l],nums[r]])
+
+                    l += 1
+                    # if the value is alrady checked
+                    while (l < r and nums[l] == nums[l-1]):
+                        l += 1            
+
+        return result    
