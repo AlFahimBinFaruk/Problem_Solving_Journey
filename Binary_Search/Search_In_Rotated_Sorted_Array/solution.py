9fe7a7e2c6af4869fe6aa0f9b91c401472b1f7c9
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-11-26 19:28:03 +0600

    updated

--- Binary_Search/Search_In_Rotated_Sorted_Array/solution.py
+++ Binary_Search/Search_In_Rotated_Sorted_Array/solution.py
@@ -1,30 +1,29 @@
 # https://leetcode.com/problems/search-in-rotated-sorted-array/
-# Time O(log(N)) | Space O(1)
-def search(nums, target):
-    l = 0
-    r = len(nums)-1
+class Solution:
+    # Time O(logN) | Space O(1)
+    def search(self, nums: List[int], target: int) -> int:
+        # Binary Search
+        # as we will rotate the given sorted array ,there will be two sorted array(left sorted portion,right sorted portion.)
+        l = 0
+        r = len(nums) -1
 
-    while l <= r:
-        mid = (l+r)//2
+        while l <= r:
+            mid = (l+r) // 2
 
-        if target == nums[mid]:
-            return mid
+            if nums[mid] == target:
+                return mid
 
-        # Search left partition
-        if nums[l] <= nums[mid]:
-            if target > nums[mid] or target < nums[l]:
-                l = mid+1
+            if nums[l] <= nums[mid]:
+                # Search in left sorted portion
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
             else:
-                r = mid-1
-        # search right partition
-        else:
-            if target < nums[mid] or target > nums[r]:
-                r = mid-1
-            else:
-                l = mid+1
+                # search in right sorted portion
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
 
-    return -1
-
-
-print(search([4, 5, 6, 7, 0, 1, 2], 0))
-print(search([4, 5, 6, 7, 0, 1, 2], 3))
+        return -1    
