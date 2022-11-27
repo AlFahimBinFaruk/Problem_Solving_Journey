6eaee6ceb64bc32f81e36b0fec13802013b9d433
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-11-27 18:53:38 +0600

    updated

--- Binary_Search/Find_Minimum_In_Rotated_Sorted_Array/solution.py
+++ Binary_Search/Find_Minimum_In_Rotated_Sorted_Array/solution.py
@@ -1,25 +1,29 @@
 # https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
-# Time O(log(N)) | Space O(1)
-def findMin(nums):
-    result=nums[0]
+class Solution:
+    # Time O(logN)  | Space O(1)
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
 
-    l=0
-    r=len(nums)-1
+        result = float("infinity")
 
-    while l<=r:
-        mid=(l+r)//2
-        # if the array is sorted
-        if nums[l]<nums[r]:
-            result=min(result,nums[l])
+        while l <= r:
+            # if the array is sorted
+            if nums[l] < nums[r]:
+                result = min(result,nums[l])
+                # it makes the code more optimized.
+                break
+                
 
-        result = min(result,nums[mid])
+            mid = (l+r) // 2
+            result = min(result,nums[mid])
+            # if the array is part of left sorted portion 
+            # we will search in right sorted portion
+            if nums[mid] >= nums[l]:
+                l = mid + 1
+            else:
+                # or else we will search in the right sorted portion        
+                r = mid - 1
 
-        if nums[mid]>=nums[l]:
-            l = mid+1
-        else:
-            r = mid-1
 
-    return result
-
-print(findMin([4, 5, 6, 7, 0, 1, 2]))
-print(findMin([3, 4, 5, 1, 2]))                  
+        return result        
