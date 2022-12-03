cd521cab4fc874b419465297cff068b46cff1155
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-12-03 18:59:13 +0600

    updated

--- Linked_List/Reorder_List/solution.py
+++ Linked_List/Reorder_List/solution.py
@@ -5,41 +5,59 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-
-# Time O(N) | Space O(1)
 class Solution:
+    # Time O(N) | Space O(1)
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
+
+        ***
+        the main concept is we will split the list in 1st and 2nd half.
+        then we will keep changing the pointers by merging those list 1st half from starting to end , 2nd half from end to start.
+        ***
         """
+        # they want us to reorder in = 0th node -> last node -> 1st node -> 2nd last node -> 2nd node...
+        # we gonna use slow,fast pointer to solve this.
+
         slow = head
         fast = head.next
-        # Find the middle of the list
+
+        # find the middle of the list as we gonna split the list into two 
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        # Reverse the second half
-        second = slow.next
+        # determine the 2nd half of the list 
+        secondList = slow.next
+        # as slow.next will be the beginning of second list the first list slow.next will be null    
         slow.next = None
+
+        # reverse the 2nd half(as we will traverse the 1st half from start to end and 2nd half from end to start).
         prev = None
 
-        while second:
-            tmp = second.next
-            second.next = prev
-            prev = second
-            second = tmp
+        while secondList:
+            tmpNxt = secondList.next
+            secondList.next = prev
+            prev = secondList
+            secondList = tmpNxt
 
-        # Merge the both list
-        first = head
-        second = prev
-        while second:
-            temp1 = first.next
-            temp2 = second.next
+        # merge two list    
+        firstListNode = head
+        secondListNode = prev
 
-            first.next = second
-            second.next = temp1
+        while secondListNode:
+            firstTmp = firstListNode.next
+            secondTmp = secondListNode.next
+            
+            # updating the nodes.
+            firstListNode.next = secondListNode
+            secondListNode.next = firstTmp
 
-            first = temp1
-            second = temp2
+            firstListNode = firstTmp
+            secondListNode = secondTmp
+
+
+
+
+
+
