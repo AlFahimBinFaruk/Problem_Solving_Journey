4c7a82ff92210862bcd2e76c4b47e2a6aefb6c53
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-12-04 18:08:08 +0600

    updated

--- Linked_List/Linked_List_Cycle/solution.py
+++ Linked_List/Linked_List_Cycle/solution.py
@@ -3,18 +3,19 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
-# Time O(N) | Space O(1)
 class Solution:
+    # Time O(N) | Space O(1)
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # we gonna use slow and fast pointer to solve this.
+        # the main concept is we gonna see if slow == fast if it match means there is a cycle.
         slow = head
         fast = head
 
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next.next
+            fast= fast.next.next    
 
             if slow == fast:
                 return True
 
-        return False
+        return False      
