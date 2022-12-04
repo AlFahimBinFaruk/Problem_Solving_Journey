fccefedabbcfad3cf969c3e66410cbaabd9c4f2f
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-12-04 19:05:23 +0600

    updated

--- Linked_List/Merge_K_Sorted_List/solution.py
+++ Linked_List/Merge_K_Sorted_List/solution.py
@@ -3,46 +3,56 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-# Time O(N*log(k)) | Space O(N+K)
-# More efficeint than solution two(O(N)) in python.
-class SolutionOne:
+class Solution:
+    # Time O(N*Log(K)) | Space O(N + K)
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists and len(lists)==0:
+        if not lists or len(lists) == 0:
             return None
-
-        while len(lists)>1:
-            mergedList=[]
+         
+        # if length is 1 then the 0 indexed linked list is the answer. 
+        while len(lists) > 1:
+            mergedList = []
 
             for i in range(0,len(lists),2):
-                l1=lists[i]
-                l2=lists[i+1] if i+1 < len(lists) else None
-                mergedList.append(self.mergeTwoLists(l1,l2))
+                l1 = lists[i]
+                l2 = lists[i+1] if i+1 < len(lists) else None
+                mergedList.append(self.mergeSortedList(l1,l2))
+            
+            lists = mergedList   
+             
+        return lists[0]       
 
-            lists=mergedList
 
-        return lists[0]   
-
-    def mergeTwoLists(self,l1, l2):
+    
+    # Time O(N) | Space O(N)
+    def mergeSortedList(self,l1,l2):
         dummy = ListNode()
-        tail = dummy
+        head = dummy
 
         while l1 and l2:
             if l1.val < l2.val:
-                tail.next = l1
+                head.next = l1
                 l1 = l1.next
             else:
-                tail.next = l2
+                head.next = l2
                 l2 = l2.next
 
-            tail = tail.next
+            head = head.next
 
+
+        # handling edge case of if any of l1 or l2 remains empty
         if l1:
-            tail.next = l1
-        if l2:
-            tail.next = l2
+            head.next = l1
 
-        return dummy.next       
+        if l2:
+            head.next = l2
+
+        return dummy.next                
+
+
+
+
+    
 
 
 
