493f5fe7126c24337b0591a603655c2da52b617a
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-12-07 20:58:45 +0600

    updated

--- Tree/Subtree_Of_Another_Tree/solution.py
+++ Tree/Subtree_Of_Another_Tree/solution.py
@@ -4,28 +4,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# Time O(N*M) | Space O(1)
 class Solution:
+    # Time O(N*M) | Space O(N+M)
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        # If subRoot is null wheather subTree of root is null or not it will always return true
+        # DFS - recursion
+        
+        # if there is not subroot (subroot is Null) then it will always return True because we then have to look for null in root and we are sure that there will be a null in root.
         if not subRoot:
             return True
+
+        # if subroot is not null but root is null then we will return False becoz we have no root to match subroot.
         if not root:
             return False
 
-        if self.sameTree(root, subRoot):
+
+        # see if the structure of root and subRoot match.if match return True from there and end the algo.
+        if self.sameTree(root,subRoot):
+            return True   
+        
+        # then search in both left and right subtree to see if the subRoot matches.
+        return (self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot))         
+
+
+
+    
+    # Time O(N) | Space O(N)
+    def sameTree(self,p,q):
+        if not p and not q:
             return True
 
-        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
-
-    def sameTree(self, root, subRoot):
-        if not root and not subRoot:
-            return True
-
-        if root and subRoot and root.val == subRoot.val:
-            isSame = self.sameTree(root.left, subRoot.left) and self.sameTree(
-                root.right, subRoot.right)
+        if (p and q) and (p.val == q.val):
+            isSame = (self.sameTree(p.left,q.left) and self.sameTree(p.right,q.right))
             return isSame
         else:
-            return False
+            return False            
+                 
