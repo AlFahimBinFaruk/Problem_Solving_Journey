f8028ca2ae8d9c0e074906b4de88b92905cfe1df
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-12-08 19:34:58 +0600

    updated

--- Tree/Validate_Binary_Search_Tree/solution.py
+++ Tree/Validate_Binary_Search_Tree/solution.py
@@ -4,17 +4,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# Time O(N) | Space O(H) H-the amount of space the recursion needs to execute
 class Solution:
+    # Time O(N) | Space O(N)
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def isValid(node, left, right):
-            if not node:
-                return True
-
-            if not (node.val > left and node.val < right):
-                return False
-
-            return (isValid(node.left, left, node.val) and isValid(node.right, node.val, right))
-
-        return isValid(root, float("-inf"), float("inf"))
+        return self.validTree(root,float("-inf"),float("inf"))
+    
+    def validTree(self,root,left,right):
+        # DFS - recursion
+        # if we get a null root we will return true,because technically an empty binary search tree is still a valid binary search tree.
+        if not root:
+            return True
+        
+        # see if they match binary search tree conditions.
+        if not (left < root.val and right > root.val):
+            return False
+        
+        # the left and right subtree mush also be valid b-s-t 
+        # while checking left subtree we will always keep the left -inf
+        # while checking right subtree we will always keep the right inf
+        return (self.validTree(root.left,left,root.val) and self.validTree(root.right,root.val,right))  
