965fc5adca14c96000a8e1a7850dedbebad851ac
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-12-10 20:02:16 +0600

    updated

--- Tree/Construct_Binary_Tree_From_Preorder_And_Inorder_Traversal/solution.py
+++ Tree/Construct_Binary_Tree_From_Preorder_And_Inorder_Traversal/solution.py
@@ -5,15 +5,22 @@
 #         self.left = left
 #         self.right = right
 
-# Time O(N^2) | Space O(H)
 class Solution:
+    # Time O(N^2) | Space O(N)
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # DFS - recursion
+        # we have to return the binray tree in preorder
         if not preorder or not inorder:
             return None
-
+         
+        # create root node 
         root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+
+        # find the value of that preorder[0] in inorder
+        mid =  inorder.index(preorder[0])
+        
+        # Defines tree left and right nodes
+        root.left = self.buildTree(preorder[1:mid+1],inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:])
 
         return root
