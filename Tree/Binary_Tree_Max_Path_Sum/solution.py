d261ee00665d274b5d38e8be068ff062c135370b
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-12-11 18:59:42 +0600

    updated

--- Tree/Binary_Tree_Max_Path_Sum/solution.py
+++ Tree/Binary_Tree_Max_Path_Sum/solution.py
@@ -4,28 +4,29 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# Time O(N) | Space O(H)
 class Solution:
+    # Time O(N) | Space O(H)
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        # DFS - recursion
+        # we have to monitor max path of every recursion.
+        # we will update res depending on every recursion call if the max path value is 
+        # higher than before as path does not need to pass through the root. 
         res = [root.val]
 
         def dfs(root):
             if not root:
                 return 0
-
-            leftMax = dfs(root.left)
-            rightMax = dfs(root.right)
-
-            leftMax = max(leftMax, 0)
-            rightMax = max(rightMax, 0)
             
-            # Calculate the res with split
-            res[0] = max(res[0], root.val+leftMax+rightMax)
+            # if we get any negative values we will not include them as including childrens is optional.
+            leftSum = max(dfs(root.left),0)
+            rightSum = max(dfs(root.right),0)
+            
+            # update res by splitting the tree split
+            res[0] = max(res[0],(root.val+leftSum+rightSum))
 
-            # Return the max value without split.
-            return root.val+max(leftMax, rightMax)
+            # return response without splitting the tree
+            return (root.val + max(leftSum,rightSum))
 
         dfs(root)
 
-        return res[0]
+        return res[0]     
