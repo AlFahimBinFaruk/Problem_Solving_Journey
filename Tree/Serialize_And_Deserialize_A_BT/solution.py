e658328caaee4ed0e05198ee5e4aa740a9f56def
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-12-11 20:11:53 +0600

    updated

--- Tree/Serialize_And_Deserialize_A_BT/solution.py
+++ Tree/Serialize_And_Deserialize_A_BT/solution.py
@@ -5,56 +5,59 @@
 #         self.left = None
 #         self.right = None
 
-
 class Codec:
-    # Time O(N) | Space O(N)
+    # Time O(N) | Space O(H) 
     def serialize(self, root):
         """Encodes a tree to a single string.
-
+        
         :type root: TreeNode
         :rtype: str
         """
-        res = []
+        res  = []
 
-        def dfs(node):
-            if not node:
+        # DFS - recursion
+        def dfs(root):
+            if not root:
                 res.append("N")
                 return
 
-            res.append(str(node.val))
-            dfs(node.left)
-            dfs(node.right)
+            res.append(str(root.val))
+            dfs(root.left)
+            dfs(root.right)
 
         dfs(root)
-        print("val", ",".join(res))
-        return ",".join(res)
 
-    # Time O(N) | Space O(N)
+        return ",".join(res)        
 
+           
+        
+    # Time O(N) | Space (H)
     def deserialize(self, data):
         """Decodes your encoded data to tree.
-
+        
         :type data: str
         :rtype: TreeNode
         """
-        vals = data.split(",")
+        data =  data.split(",")
         self.i = 0
 
+        # DFS - recursion
         def dfs():
-            if vals[self.i] == "N":
+            if data[self.i] == "N":
                 self.i += 1
                 return None
 
-            node = TreeNode(int(vals[self.i]))
+            newNode = TreeNode(int(data[self.i]))
             self.i += 1
-            node.left = dfs()
-            node.right = dfs()
 
-            return node
+            newNode.left = dfs()
+            newNode.right = dfs() 
 
-        return dfs()
+            return newNode
 
+        return dfs()       
 
+        
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
