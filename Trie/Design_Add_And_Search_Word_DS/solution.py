308178715b27ac2196d32de671da66cc544ebe09
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-12-12 19:56:18 +0600

    updated

--- Trie/Design_Add_And_Search_Word_DS/solution.py
+++ Trie/Design_Add_And_Search_Word_DS/solution.py
@@ -1,52 +1,56 @@
+# We gonna use Trie DS to solve this
 class TrieNode:
     def __init__(self):
+        # key = node , val = child
         self.children = {}
         self.endOfWord = False
 
-
 class WordDictionary:
-
     def __init__(self):
         self.root = TrieNode()
         self.maxWordLength = 0
-
-    # Time O(N) | Space O(N)
+        
+    
+    # Time O(N) | Space O(1)
     def addWord(self, word: str) -> None:
         curr = self.root
 
         for c in word:
-            if c not in curr.children:
+            if not c in curr.children:
                 curr.children[c] = TrieNode()
             curr = curr.children[c]
-        curr.endOfWord = True
-        self.maxWordLength = max(self.maxWordLength, len(word))
-
-    # Time O(N) | Space O(N)
+        curr.endOfWord = True 
+        # we will always keep track which is the biggest word in our trie to optimized the code
+        self.maxWordLength = max(self.maxWordLength,len(word))       
+        
+    
+    # Time O(N) | Space O(1)
     def search(self, word: str) -> bool:
-        # In order to prevent searching a word that length is greater than any word i have store so we can immediately return False in order for optimization.
+
         if len(word) > self.maxWordLength:
             return False
 
-        def dfs(j, root):
+        def dfs(j,root):
             curr = root
 
-            for i in range(j, len(word)):
+            for i in range(j,len(word)):
                 c = word[i]
-
+                
+                # if the char is . then we have to match every child of that node to make sure its valid or not
                 if c == ".":
                     for child in curr.children.values():
-                        if (dfs(i+1, child)):
+                        if (dfs(i+1,child)):
                             return True
-                    return False
-
+                    # only after we have matched every child and didnt find any pattern we will return false.        
+                    return False    
                 else:
                     if c not in curr.children:
                         return False
-                    curr = curr.children[c]
+                    curr = curr.children[c]   
 
-            return curr.endOfWord
+            return curr.endOfWord  
 
-        return dfs(0, self.root)
+        return dfs(0,self.root)                
 
 
 # Your WordDictionary object will be instantiated and called as such:
