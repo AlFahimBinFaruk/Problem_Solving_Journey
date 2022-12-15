f40bb4a5598b143baf3d5a83afdadcba2e138bdf
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-12-15 11:11:21 +0600

    Update solution.py

--- Backtracking/Combanation_Sum/solution.py
+++ Backtracking/Combanation_Sum/solution.py
@@ -1,20 +1,27 @@
 class Solution:
-    # Time O(2^t) | Space
+    # Time O(2^t) | Space O(N)
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # we gonna use DFS and desicion tree(takes two decision) concept to solve this
         result = []
 
-        def dfs(i, curr, total):
+        def dfs(i,curr,total):
             if total == target:
                 result.append(curr.copy())
                 return
 
-            if i >= len(candidates) or total > target:
+            # handle outof bound
+            if (i >= len(candidates)) or total > target:
                 return
-
+            
+            # first decision
             curr.append(candidates[i])
-            dfs(i, curr, total+candidates[i])
-            curr.pop()
-            dfs(i+1, curr, total)
+            dfs(i,curr,(total+candidates[i]))
 
-        dfs(0, [], 0)
-        return result
+            # second decision
+            curr.pop()
+            dfs(i+1,curr,total)
+
+        dfs(0,[],0)
+
+        return result    
+                  
