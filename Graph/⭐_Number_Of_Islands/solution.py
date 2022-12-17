e34a324e0d2b61521b01c85e8ed58ab76839e8d1
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2022-12-17 18:49:36 +0600

    Update solution.py

--- Graph/⭐_Number_Of_Islands/solution.py
+++ Graph/⭐_Number_Of_Islands/solution.py
@@ -27,3 +27,44 @@ class Solution:
                     dfs(r, c)
 
         return isIsland
+
+    
+    ### BFS solution
+    class Solution:
+    # Time O(M * N) | Space O(N*M)
+    def numIslands(self, grid: List[List[str]]) -> int:
+        # we gonna use Breadth first search to solve this.
+        if not grid:
+            return 0
+
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        
+        visit = set()
+        isIsland = 0
+
+        def bfs(r,c):
+            q = collections.deque()
+            q.append((r,c))
+            visit.add((r,c))
+
+            while q:
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
+                row , col = q.popleft()
+
+                for dr , dc in directions:
+                    r = row + dr
+                    c = col + dc
+
+                    if r in range(ROWS) and c in range(COLS) and grid[r][c] == "1" and (r,c) not in visit:
+                        q.append((r,c))
+                        visit.add((r,c)) 
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r,c) not in visit:
+                    bfs(r,c)   
+                    isIsland += 1     
+        
+        return isIsland
+                       
