f7ab9ea78ea93e2eaefef4c163c40300e0ff9869
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2023-01-06 20:55:41 +0600

    Update 4sum.py

--- Two_Pointers/4sum.py
+++ Two_Pointers/4sum.py
@@ -6,7 +6,7 @@ class Solution:
         results = []
 
         def kSum(l,r,k,target,result,results):
-            if r-l+1 < k or k < 2 or nums[l]*k > target or  nums[r]*k < target:
+            if (r-l+1) < k or k < 2:
                 return
             if k == 2:
                 while l < r:
@@ -21,9 +21,9 @@ class Solution:
                             l += 1
             else:
                 for i in range(l,r+1):
-                    if i == l or (i > l and nums[i] != nums[i-1]):
-                        kSum(i+1,r,k-1,target-nums[i],result+[nums[i]],results)                        
-                 
-
-        kSum(0,len(nums)-1,4,target,[],results)    
-        return results
+                    if i > l and nums[i] == nums[i-1]:
+                        continue
+                    kSum(i+1,r,k-1,target-nums[i],result+[nums[i]],results)                               
+         
+        kSum(0,len(nums)-1,4,target,[],results) 
+        return results    
