2bb2f3c268d3ad91175917be35ec8825b6689248
Al Fahim Bin Faruk <85015121+AlFahimBinFaruk@users.noreply.github.com>
2023-01-10 19:22:38 +0600

    Create daily_temperatures.py

---
+++ Stack/daily_temperatures.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/daily-temperatures/
+# Time O(N^2)
+# class Solution:
+#     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+#         result = [0]*len(temperatures)
+#         for i in range(len(temperatures)):
+#             for j in range(i+1,len(temperatures)):
+#                 if temperatures[j] > temperatures[i]:
+#                     result[i] = j-i
+#                     break
+#         return result  
+
+# Time O(N)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:  
+        result = [0] * len(temperatures)
+        stack = [] # [index,temp]
+
+        for i,t in enumerate(temperatures):
+            while stack and stack[-1][1] < t:
+                popIn,popTemp = stack.pop()
+                result[popIn] = i - popIn
+            stack.append([i,t])
+
+        return result 
